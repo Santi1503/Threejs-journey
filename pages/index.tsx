73e6bebca567224ef63d1d79057420179d8ca082
Santiago Gomez de la Torre <gomezdelatorre15@gmail.com>
2023-11-27 21:23:16 +0100

    ADDING styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-import { Scene, WebGL1Renderer, PerspectiveCamera, Camera, BoxGeometry, MeshBasicMaterial, Mesh } from "three"
+import { Scene, WebGL1Renderer, PerspectiveCamera, Camera, BoxGeometry, MeshBasicMaterial, Mesh, SphereGeometry, TextureLoader, MeshPhongMaterial, BackSide, AmbientLight, HemisphereLight } from "three"
 
 function HomePage() {
     useEffect(() => {
@@ -17,8 +17,21 @@ function HomePage() {
         const geometry = new BoxGeometry(1,1,1)
         const material = new MeshBasicMaterial({color: 0xffffff })
         const cube = new Mesh(geometry, material)
-
         scene.add(cube)
+
+        const skyGeometry = new SphereGeometry(360, 25, 25)
+        const loader = new TextureLoader()
+        const texture = loader.load("/custom-sky.png")
+        const material2 = new MeshPhongMaterial({
+            map: texture
+        })
+        const skybox = new Mesh(skyGeometry, material2)
+        scene.add(skybox)
+        skybox.material.side = BackSide
+
+        scene.add(new AmbientLight(0xffffff, 0.8))
+        scene.add(new HemisphereLight(0xffffff, 0.8))
+
 
         renderer.setSize(window.innerWidth, window.innerHeight)
 
