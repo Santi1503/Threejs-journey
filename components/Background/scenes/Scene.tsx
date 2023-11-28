d67943180651681ea0532793e0ac74b3bc7661ea
Santiago Gómez de la Torre <gomezdelatorre15@gmail.com>
2023-11-28 09:12:42 +0100

    NEW FEATURES

--- components/Background/scenes/Scene.tsx
+++ components/Background/scenes/Scene.tsx
@@ -2,15 +2,19 @@ import { Scene as SceneThree } from "three"
 import Camera from "../classes/Camera"
 import Lights from "../classes/Ligths"
 import Skybox from "../classes/Skybox"
+import Loader from "../classes/Loader"
+import Logo from "../classes/Objects/Logo"
 
 export default class Scene extends SceneThree {
     private camera: Camera
 
     constructor(renderer) {
         super()
+        const loader = new Loader()
         this.camera = new Camera()
         new Lights(this)
         new Skybox(this)
+        new Logo(this, loader)
         this.update(renderer)
     }
 
