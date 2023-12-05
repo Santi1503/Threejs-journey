5c02755371be6bb8d31acd62ef482f7c67a6a99a
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2023-12-05 09:47:07 +0100

    ADJUSTMENTS

--- components/Background/classes/Camera.tsx
+++ components/Background/classes/Camera.tsx
@@ -13,13 +13,24 @@ export default class Camera extends PerspectiveCamera {
     }
 
     private moveCamera(){
-        const scrollTop = this.container.scrollTop
-        const scrollHeight = this.container.scrollHeight
-        const offsetHeight = this.container.offsetHeight
-        const t = -scrollTop*100 / (scrollHeight - offsetHeight)
+        if (window.innerWidth > 1500) {
+            const scrollTop = this.container.scrollTop
+            const scrollHeight = this.container.scrollHeight
+            const offsetHeight = this.container.offsetHeight
+            const t = -scrollTop*100 / (scrollHeight - offsetHeight)
 
-        this.position.z = t * 0.35
-        this.position.x = t * 0.025
-        this.rotation.y = t * 0.025
+            this.position.z = t * 0.35
+            this.position.x = t * 0.025
+            this.rotation.y = t * 0.025
+        } else {
+            const scrollTop = this.container.scrollTop
+            const scrollHeight = this.container.scrollHeight
+            const offsetHeight = this.container.offsetHeight
+            const t = -scrollTop*17 / (scrollHeight - offsetHeight)
+
+            this.position.z = t * 0.035
+            this.position.x = t * 0.025
+            this.rotation.y = t * 0.025
+        }
     }
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/main.css"
 import "../styles/sections.css"
-import "../styles/header.css"
+import "../styles/navbar.css"
 
 function MyApp({ Component, pageProps}) {
     return <Component {...pageProps} />
